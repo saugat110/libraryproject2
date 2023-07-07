6e6b54dc493ca8fc288615c50c4b8a0347a5ee63
saugat sthapit <saugatsthapit3@gmail.com>
2023-07-07 19:23:46 +0545

    book search complete

--- search/book_search.php
+++ search/book_search.php
@@ -3,7 +3,7 @@
     if(isset($_POST['input'])){
         require_once '../config/connection.php';
 
-        $query = "select * from author where lower(Name) like concat(:search, '%') or auth_id like concat(:search, '%')";
+        $query = "select * from books where lower(name) like concat(:search, '%') or  isbn like concat(:search, '%') or lower(category) like concat(:search, '%') or lower(author) like concat(:search, '%') or lower(rack) like concat(:search, '%')";
         $stmt = $db -> prepare($query);
         $stmt -> execute([':search' => strtolower($_POST['input'])]);
         $num_rows = $stmt -> rowCount();
@@ -13,25 +13,36 @@
         if($num_rows > 0){ ?>
             
 
-            <table id="result_table" >
-                <tr>
-                    <th>Author_ID</th>
-                    <th>Author Name</th>
+            <table id="result_table" >  <!-- default table ra result table ko code same id class sabai -->
+                
+            <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>ISBN</th>
+                    <th>Category</th>
+                    <th>Author</th>
+                    <th>Rack</th>
+                    <th>Copies</th>
                     <th>Action</th>
                 </tr>
-                <?php foreach($result as $single) {?>
+                <?php foreach ($result as $single) { ?>
                     <tr>
-                        <td><?php echo $single['auth_id']; ?></td>
-                        <td><?php echo $single['Name']; ?></td>
+                        <td class="image_cell"><img src="../images/books/<?php echo $single['imgname']; ?>"></td>
+                        <td><?php echo $single['name']; ?></td>
+                        <td><?php echo $single['isbn']; ?></td>
+                        <td><?php echo $single['category']; ?></td>
+                        <td><?php echo $single['author']; ?></td>
+                        <td><?php echo $single['rack']; ?></td>
+                        <td><?php echo $single['copies']; ?></td>
                         <td>
-                            <button type="button" onclick="updateform('<?php echo $single['auth_id'];?>', '<?php echo $single['Name'];?>')" id="search_edit_button">Edit</button>
+                            <button type="button" onclick="updateform('<?php echo $single['auth_id']; ?>', '<?php echo $single['Name']; ?>')" id="search_edit_button">Edit</button>
                             <form action="../UD/delete.php" method="post">
-                                <input type="hidden" name="author_id" value="<?php echo $single['auth_id'];?>">
-                                <input type="submit" value="Delete" name="delete_author">
+                                <input type="hidden" name="b_id" value="<?php echo $single['b_id']; ?>">
+                                <input type="submit" value="Delete" name="delete_book">
                             </form>
                         </td>
                     </tr>
-                <?php }?>
+                <?php } ?>
 
             </table>
 
